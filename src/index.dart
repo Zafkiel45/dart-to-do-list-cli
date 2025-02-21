68365576817edaee4006ddef352716e44a03b3ff
zafkiel45 <zafkielc@gmail.com>
2025-02-21 17:03:38 -0300

    initial commit

---
+++ src/index.dart
@@ -0,0 +1,26 @@
+import "dart:io"; // Like FileSystem in Node.js
+void main(List<String> arguments) {
+  
+} 
+
+class Application {
+  final List<String> arguments;
+
+  Application(this.arguments); 
+}
+
+class addTask {
+
+}
+
+class deleteTask {
+
+}
+
+class addDeadline {
+
+}
+
+class createList {
+  
+}
