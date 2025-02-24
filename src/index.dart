e47a740dac586c7710d1f8730720ef92ac7880f4
zafkiel45 <zafkielc@gmail.com>
2025-02-24 16:48:59 -0300

    add task working

--- src/index.dart
+++ src/index.dart
@@ -1,26 +1,70 @@
 import "dart:io"; // Like FileSystem in Node.js
 void main(List<String> arguments) {
-  
+  Application app = new Application(arguments);
+
+  app.executeProgram();
 } 
 
 class Application {
+
   final List<String> arguments;
+  late Object operation;
 
-  Application(this.arguments); 
+  Application(this.arguments);
+
+  void executeProgram() {
+
+    switch(arguments[0]) {
+      case "add": 
+        AddTask operation = AddTask(arguments[1], arguments[2]);
+        operation.HandleAddTask();
+      default: 
+        throw "None correspondent operation";
+    };
+  }
 }
 
-class addTask {
+class AddTask {
+  final String listName; 
+  final String ItemName;
+
+  AddTask(this.listName, this.ItemName);
+
+  void HandleAddTask() async {
+    final File list = File('./lists/${listName}.json');
+
+    if(list.existsSync()) {
+      list.writeAsString('$ItemName', mode: FileMode.append);
+      print('I am writting in the file!');
+    } else {
+      CreateList? newList = new CreateList(listName);
+      print('I am creating and writting in the file!');
+
+      newList.HandleCreateList();
+
+      await list.writeAsString('$ItemName', mode: FileMode.append);
+    }
+  }
+}
+
+class DeleteTask {
 
 }
 
-class deleteTask {
+class AddDeadline {
 
 }
 
-class addDeadline {
+class CreateList {
+  final String listName; 
 
-}
+  CreateList(this.listName);
 
-class createList {
-  
+  void HandleCreateList() async {
+    File list = File('./lists/${this.listName}.json');
+
+    await list.create();
+
+    print('list: ${list.path} was created successfuly!');
+  }
 }
