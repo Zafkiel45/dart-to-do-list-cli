92383693600156694819973e70a3dc1fa16e8e32
zafkiel45 <zafkielc@gmail.com>
2025-02-27 16:29:29 -0300

    refatore: restructured getDate util function

--- utils/validateDate.dart
+++ utils/validateDate.dart
@@ -1,17 +1,15 @@
-bool validateDate(String date) {
-  try {
-    if(date.isEmpty) return false;
-  
-    final RegExp pattern = RegExp(r"^(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])-(\d{4})$"); 
-  
-    return pattern.hasMatch(date);
-  } catch (err, stack) {
-    throw "❗❗❗ An error occured: ➡️ $err ➡️ $stack";
-  }
-}
-
 String getDate(String date) {
-  if(validateDate(date)) return date;
-  else if(date == "no-date") return "";
-  else return throw "Invalid Date... Please, use the format: [MM]-[DD]-[YYYY]";
+  try {
+    final RegExp pattern = RegExp(
+      r"^(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])-(\d{4})$",
+    );
+
+    if (pattern.hasMatch(date))
+      return date;
+    else
+      return "";
+  } catch (err, stack) {
+    print('❌ $err \n ❌ $stack');
+    throw "❌ Invalid Date... Please, use the format: [MM]-[DD]-[YYYY]";
+  }
 }
